04b267bdd183758a28ee87d63efefa61e95f658a
Robin Kahlow <tora@warlock.ai>
2024-11-10 00:16:27 +0000

    save flops counter callback state

--- discrete_flow_matching/flops.py
+++ discrete_flow_matching/flops.py
@@ -9,14 +9,37 @@ logger = get_logger()
 
 class FlopCounterCallback(pl.Callback):
     def __init__(self, train_step_flops: float | None = None):
-        self.t_start = None
+        # Constants
         self.device_flops_per_second = 142.5e12  # RTX 3090 142.5 TFLOPS bf16
+        self.train_step_flops = train_step_flops
+
+        # Needed for calculating durations
+        self.t_start = None
+        self.previous_t_train_batch_end = None
+        self.train_batch_t_start = None
+
+        # Dynamic flop counter
         self.flop_counter = None
+
+        # State
         self.trained_flops = 0
         self.trained_optimal_flops = 0
-        self.train_batch_t_start = None
         self.trained_duration = 0
-        self.train_step_flops = train_step_flops
+        self.duration = 0
+
+    def load_state_dict(self, state_dict):
+        self.trained_flops = state_dict.get("trained_flops", 0)
+        self.trained_optimal_flops = state_dict.get("trained_optimal_flops", 0)
+        self.trained_duration = state_dict.get("trained_duration", 0)
+        self.duration = state_dict.get("duration", 0)
+
+    def state_dict(self):
+        return dict(
+            trained_flops=self.trained_flops,
+            trained_optimal_flops=self.trained_optimal_flops,
+            trained_duration=self.trained_duration,
+            duration=self.duration,
+        )
 
     def on_fit_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
         if self.train_step_flops is None:
@@ -51,9 +74,15 @@ class FlopCounterCallback(pl.Callback):
         train_step_optimal_flops = train_step_duration * self.device_flops_per_second
         self.trained_optimal_flops += train_step_optimal_flops
 
+        # Accumulate total runtime
+        if self.previous_t_train_batch_end is not None:
+            self.duration += t - self.previous_t_train_batch_end
+        else:
+            self.duration += t - self.t_start
+        self.previous_t_train_batch_end = t
+
         # Optimal total flops
-        duration = t - self.t_start
-        optimal_flops = duration * self.device_flops_per_second
+        optimal_flops = self.duration * self.device_flops_per_second
 
         # MFU
         trained_mfu = self.trained_flops / self.trained_optimal_flops
@@ -61,16 +90,32 @@ class FlopCounterCallback(pl.Callback):
 
         # Trained duration fraction
         self.trained_duration += train_step_duration
-        trained_duration_fraction = self.trained_duration / duration
+        trained_duration_fraction = self.trained_duration / self.duration
 
         self.log_dict(
             {
-                "train/step_pflops": self.train_step_flops / 1e15,
-                "train/step_optimal_pflops": train_step_optimal_flops / 1e15,
-                "train/trained_pflops": self.trained_flops / 1e15,
-                "train/trained_optimal_pflops": self.trained_optimal_flops / 1e15,
-                "train/trained_mfu": trained_mfu,
-                "mfu": mfu,
-                "train/trained_duration_fraction": trained_duration_fraction,
+                "flops/train_step_pflops": self.train_step_flops / 1e15,
+                "flops/train_step_optimal_pflops": train_step_optimal_flops / 1e15,
+                "flops/trained_pflops": self.trained_flops / 1e15,
+                "flops/trained_optimal_pflops": self.trained_optimal_flops / 1e15,
+                "flops/trained_mfu": trained_mfu,
+                "flops/trained_duration_fraction": trained_duration_fraction,
+                "flops/train_duration": self.trained_duration,
+                "flops/duration": self.duration,
+                "flops/mfu": mfu,
             }
         )
+
+    def on_validation_batch_end(self, *args, **kwargs) -> None:
+        if self.train_step_flops is not None:
+            assert self.trained_flops is not None
+            assert self.trained_optimal_flops is not None
+            self.log_dict(
+                {
+                    "flops/train_step_pflops": self.train_step_flops / 1e15,
+                    "flops/trained_pflops": self.trained_flops / 1e15,
+                    "flops/trained_optimal_pflops": self.trained_optimal_flops / 1e15,
+                    "flops/train_duration": self.trained_duration,
+                    "flops/duration": self.duration,
+                }
+            )
